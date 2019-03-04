ab5d2caee0f9f7d0a3038615a086c748919155b8
anjoy8 <3143422472@qq.com>
2019-03-04 14:16:21 +0800

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -140,7 +140,7 @@ export const addUser = params => {
     return axios.post(`${base}/api/user/post`, params);
 };
 export const batchRemoveUser = params => {
-    return axios.get(`${base}/user/batchremove`, {params: params});
+    // return axios.get(`${base}/user/batchremove`, {params: params});//没做
 };
 
 // 角色管理
