3d80b303638641767480b8440cbfac77218e9b00
anjoy8 <3143422472@qq.com>
2019-05-08 19:05:13 +0800

    Update _import_production.js

--- src/router/_import_production.js
+++ src/router/_import_production.js
@@ -1,6 +1,6 @@
 // 生产环境导入组件
 // NGINX 发布模式
-// module.exports = file => () => import('@/views' + file + '.vue')
+module.exports = file => () => import('@/views' + file + '.vue')
 
 // IIS 发布模式下，使用下边这个方法
-export default  file => () => import('@/views' + file + '.vue')
+// export default  file => () => import('@/views' + file + '.vue')
