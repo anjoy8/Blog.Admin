61beb3b8217414e618c265a82c901ce3336fbc42
anjoy8 <3143422472@qq.com>
2019-05-13 13:05:58 +0800

    Update My.vue

--- src/views/System/My.vue
+++ src/views/System/My.vue
@@ -3,13 +3,6 @@
 		<el-form-item label="我的昵称">
 			<el-input v-model="form.name"></el-input>
 		</el-form-item>
-		<el-form-item label="性别">
-			<el-select v-model="form.region" placeholder="请选择">
-				<el-option label="保密" value="保密"></el-option>
-				<el-option label="男" value="男"></el-option>
-				<el-option label="女" value="女"></el-option>
-			</el-select>
-		</el-form-item>
 
 		<el-form-item label="旧密码" prop="uLoginPWD">
 			<el-input v-model="editForm.uLoginPWD" type="text"  auto-complete="off"></el-input>
@@ -18,17 +11,6 @@
 			<el-input v-model="editForm.uLoginPWD" show-password  auto-complete="off"></el-input>
 		</el-form-item>
 
-		<el-form-item label="性别">
-			<el-switch on-text="" off-text="" v-model="form.delivery"></el-switch>
-		</el-form-item>
-		<el-form-item label="兴趣爱好">
-			<el-checkbox-group v-model="form.type">
-				<el-checkbox label="美食/餐厅线上活动" name="type"></el-checkbox>
-				<el-checkbox label="地推活动" name="type"></el-checkbox>
-				<el-checkbox label="线下主题活动" name="type"></el-checkbox>
-				<el-checkbox label="单纯品牌曝光" name="type"></el-checkbox>
-			</el-checkbox-group>
-		</el-form-item>
 		<el-form-item label="留言/备注">
 			<el-input type="textarea" v-model="form.desc"></el-input>
 		</el-form-item>
