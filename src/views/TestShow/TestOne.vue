74be6595e32d6fc220ea2940dfa63c28e69de5c5
anjoy8 <3143422472@qq.com>
2019-07-05 11:45:40 +0800

    Update TestOne.vue

--- src/views/TestShow/TestOne.vue
+++ src/views/TestShow/TestOne.vue
@@ -1,10 +1,27 @@
 <template>
-    <div>我是测试的第一个页面.</div>
+    <div>我是测试的第一个页面. name is {{ msg }}</div>
 </template>
 
 <script>
+    import axios from 'axios';
+
     export default {
-        name: "TestOne"
+        name: "TestOne",
+        data(){
+            return{
+                msg:''
+            }
+        },
+        mounted(){
+            let that=this;
+            axios.post('/api/Values/TestPostPara?name=anson zhang', {})
+                .then(res => {
+                    that.msg=res.data.name
+                })
+                .catch(err => {
+                    console.error(err);
+                })
+        }
     }
 </script>
 
