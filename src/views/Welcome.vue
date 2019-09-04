d6d9cd4336f418019cd4fe8ce393bd41c96d6d00
anjoy <3143422472@qq.com>
2019-09-04 11:46:35 +0800

    Update Welcome.vue

--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -2,11 +2,23 @@
     <div style="margin-top: 30px;">
           <div style="margin: 20px;"> 欢迎来到 BlogAdmin 后台管理系统!</div>
 
-<el-card>
+<el-card style="width:30%;float:left;margin-right:10px;">
     <el-aside>快来加入我们吧：</el-aside>
 
     <br>
     <img style="width: 300px;" src="../assets/QQGroup.png" />
+</el-card>
+<el-card style="width:30%;float:left;margin-right:10px;">
+    <el-aside>动态添加一个vue页面：</el-aside>
+
+    <br>
+    <img style="width: 300px;" src="http://apk.neters.club/images/addVuePages.gif" />
+</el-card>
+<el-card style="width:30%;float:left;">
+    <el-aside>快速配置接口权限：</el-aside>
+
+    <br>
+    <img style="width: 300px;" src="http://apk.neters.club/images/addpermission.gif" />
 </el-card>
     </div>
 </template>
